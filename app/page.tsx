0d743bc0b41eb7ce7cf8326b2c169d1438383500
Hamed Bahram <hamed.bahram@gmail.com>
2024-02-12 12:55:02 -0500

    Add YouTube embed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { sendGTMEvent } from '@next/third-parties/google'
+import { YouTubeEmbed } from '@next/third-parties/google'
 
 export default function Home() {
   return (
@@ -8,12 +9,26 @@ export default function Home() {
       <div className='container'>
         <h1 className='text-3xl font-bold'>Next Third Party</h1>
 
-        <button
-          className='mt-8 rounded bg-sky-500 px-4 py-2 text-white'
-          onClick={() => sendGTMEvent({ event: 'buttonClicked', value: 'xyz' })}
-        >
-          Send Event
-        </button>
+        <section className='mt-12'>
+          <h2 className='text-xl font-semibold'>Google Tag Manager</h2>
+          <button
+            className='mt-4 rounded bg-sky-500 px-4 py-2 text-white'
+            onClick={() =>
+              sendGTMEvent({ event: 'buttonClicked', value: 'xyz' })
+            }
+          >
+            Send Event
+          </button>
+        </section>
+
+        <section className='mt-12'>
+          <h2 className='mb-4 text-xl font-semibold'>YouTube Embed</h2>
+          <YouTubeEmbed
+            height={400}
+            videoid='Pz8CAbeg6Q0'
+            params='controls=0'
+          />
+        </section>
       </div>
     </section>
   )
